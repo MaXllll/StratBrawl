14ede013f86164dea2941fd9fb12b8fa9234e846
Maxime Lahaye <lahayemax@gmail.com>
2014-11-16 19:49:43 +0100

    Adding Main menu Adding Play menu

---
+++ StratBrawl_source/Assets/Scripts/Menu/CreateGameClickHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CreateGameClickHandler : MonoBehaviour {
+
+	public GameObject currentPanel;
+	public GameObject nextPanel;
+	public Text title;
+
+	public void ClickCreateButton(InputField gameName){
+		currentPanel.SetActive (false);
+		nextPanel.SetActive (true);
+		title.text = gameName.text.ToString();
+	}
+
+}
---
+++ StratBrawl_source/Assets/Scripts/Menu/PlayMenuClickHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class PlayMenuClickHandler : MonoBehaviour {
+
+	public GameObject currentPanel;
+
+	public void ClickGameButton(GameObject panelToShow){
+		currentPanel.SetActive (false);
+		panelToShow.SetActive (true);
+	}
+
+	public void ClickCreateGameButton(GameObject panelToShow){
+		currentPanel.SetActive (false);
+		panelToShow.SetActive (true);
+	}
+
+}

8ad78d1e59a487566663a4f5acdc5b35825523cb
Mathieu Harharidis <hhds.mathieu@gmail.com>
2014-12-09 01:18:33 +0100

    Avancée dans la phase de planification (choix des actions, apparition des cases selectionnables pour 'move' et 'pass' et enregistrement de celles ci dans un array)

--- StratBrawl_source/Assets/Scripts/SC_enums.cs
+++ StratBrawl_source/Assets/Scripts/SC_enums.cs
@@ -2,5 +2,5 @@
 using System.Collections;
 
 
-public enum ActionType {None, Move, Tackle, Pass}
+public enum ActionType {None, Move, Tackle, Pass, Defense}
 public enum Direction {Up, Down, Right, Left}
--- StratBrawl_source/Assets/Scripts/SC_manager_ui.cs
+++ StratBrawl_source/Assets/Scripts/SC_manager_ui.cs
@@ -1,35 +1,53 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SC_manager_ui : MonoBehaviour {
-	
+
 	[SerializeField]
-	private GameObject _GO_button_back;
+	private GameObject _GO_button_back_slots;
+	[SerializeField]
+	private GameObject _GO_button_back_types;
+
 	[SerializeField]
 	private GameObject _GO_panel_actions_slots;
 	[SerializeField]
-	private GameObject _GO_panel_actions;
-	
-	
+	private GameObject _GO_panel_actions_types;
+
 	public void Awake()
 	{
-		SetActiveButtonBack(false);
-		SetActivePanelActionsSlot(false);
+		SetActiveButtonBackSlots(false);
+		SetActiveButtonBackTypes(false);
+		SetActivePanelActionsSlots(false);
 		SetActivePanelActionsTypes(false);
 	}
-	
-	public void SetActiveButtonBack(bool b_active)
+
+	public void SetActiveButtonBackSlots(bool b_active)
 	{
-		_GO_button_back.SetActive(b_active);
+		_GO_button_back_slots.SetActive(b_active);
 	}
 
-	public void SetActivePanelActionsSlot(bool b_active)
+	public void SetActiveButtonBackTypes(bool b_active)
+	{
+		_GO_button_back_types.SetActive(b_active);
+	}
+
+	public void SetActivePanelActionsSlots(bool b_active)
 	{
 		_GO_panel_actions_slots.SetActive(b_active);
 	}
 
 	public void SetActivePanelActionsTypes(bool b_active)
 	{
-		_GO_panel_actions.SetActive(b_active);
+		_GO_panel_actions_types.SetActive(b_active);
+	}
+
+	public void SetActiveGameFieldCells(bool b_active){
+//		SC_cell[] cells = _GO_gameField.GetComponentsInChildren (SC_cell);
+//		foreach ( SC_cell cell in cells ){
+//			Button cell_button = cell.GetComponentInChildren(Button);
+//			GameObject cell_button_canvas = (GameObject) cell.GetComponentInChildren(Canvas);
+//			cell_button_canvas.SetActive(b_active);
+//		}
 	}
 }
